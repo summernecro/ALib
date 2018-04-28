07adc004699c914109954d18bd3cd10e41d01e7d
summernecro <summernecro@gmail.com>
2018-04-28 13:11:20 +0800

    OnLoadingInterf add onprogress

---
+++ lib/src/main/java/com/android/lib/base/interf/OnLoadingAdapter.java
@@ -0,0 +1,20 @@
+package com.android.lib.base.interf;
+
+//by summer on 2018-04-28.
+
+public class OnLoadingAdapter implements OnLoadingInterf{
+    @Override
+    public Void onStarLoading(Object o) {
+        return null;
+    }
+
+    @Override
+    public Void onStopLoading(Object o) {
+        return null;
+    }
+
+    @Override
+    public void onProgress(Object o) {
+
+    }
+}
